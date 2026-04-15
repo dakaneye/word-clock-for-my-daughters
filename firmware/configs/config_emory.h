3d1f418632b066343b39e07ab007a5f257998d0c
Samuel Dacanay <samjdacanay@gmail.com>
2026-04-14 17:43:59 -0700

    feat: scaffold platformio project with emory and nora envs

---
+++ firmware/configs/config_emory.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CLOCK_NAME         "EMORY"
+#define CLOCK_BIRTH_MONTH  10
+#define CLOCK_BIRTH_DAY    6
+#define CLOCK_BIRTH_YEAR   2023
+#define CLOCK_BIRTH_HOUR   18
+#define CLOCK_BIRTH_MINUTE 10
+#define CLOCK_ACROSTIC     "EMORY1062023"
+#define CLOCK_WOOD_LABEL   "birch"

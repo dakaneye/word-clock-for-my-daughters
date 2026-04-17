bdc5383c84992e4e1f98427841b2bdb85c8a2384
Samuel Dacanay <samjdacanay@gmail.com>
2026-04-16 20:27:23 -0700

    feat(firmware): wire wifi_provision into main state loop

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
+#include "wifi_provision.h"
 
 void setup() {
-  Serial.begin(115200);
-  Serial.printf("word-clock booting for target: %s\n", CLOCK_NAME);
+    Serial.begin(115200);
+    Serial.printf("word-clock booting for target: %s\n", CLOCK_NAME);
+    wc::wifi_provision::begin();
 }
 
 void loop() {
-  delay(1000);
+    wc::wifi_provision::loop();
+    delay(1);  // yield to the IDLE task so watchdog + WiFi stacks run
 }
